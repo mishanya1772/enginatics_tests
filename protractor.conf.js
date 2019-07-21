0c1d4d274e2f245ac6eb60ebce8dcb030c9eea72
mishanya1772 <mishanylepsky@gmail.com>
2019-07-21 21:04:09 +0300

    Update protractor.conf.js
    Fixes for Jenkins

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   framework: 'jasmine2',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['./specs/*'],
   useAllAngular2AppRoots: true,
   protractor: true,
